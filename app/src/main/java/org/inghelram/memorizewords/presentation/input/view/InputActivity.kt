9a5dfa9912b801212ca5870e6b88c5005daad8c7
Pieter <pi-github@inghelram.org>
2018-02-01 19:32:44 +0100

    add simple input screen

---
+++ app/src/main/java/org/inghelram/memorizewords/presentation/input/view/InputActivity.kt
@@ -0,0 +1,16 @@
+package org.inghelram.memorizewords.presentation.input.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.inghelram.memorizewords.R
+
+/**
+ * Created by pieter on 01/02/18.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
