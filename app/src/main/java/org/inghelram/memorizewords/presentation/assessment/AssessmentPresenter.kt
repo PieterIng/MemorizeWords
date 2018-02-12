f4ce758d12aaed49fa6bdf2414a77316665ce097
root <root@laptop>
2018-02-12 18:03:29 +0100

    add screen for assessment

---
+++ app/src/main/java/org/inghelram/memorizewords/presentation/assessment/AssessmentPresenter.kt
@@ -0,0 +1,12 @@
+package org.inghelram.memorizewords.presentation.assessment
+
+import be.sigmadelta.substratum.presenter.AbstractPresenter
+import be.sigmadelta.substratum.threading.Executor
+import be.sigmadelta.substratum.threading.IMainThread
+import be.sigmadelta.substratum.usecase.IUseCaseFactory
+
+/**
+ * Created by pieter on 12/02/18.
+ */
+class AssessmentPresenter(executor: Executor, mainThread: IMainThread, useCaseFactory: IUseCaseFactory) : AbstractPresenter(executor, mainThread, useCaseFactory) {
+}
---
+++ app/src/main/java/org/inghelram/memorizewords/presentation/assessment/view/AssessmentActivity.kt
@@ -0,0 +1,9 @@
+package org.inghelram.memorizewords.presentation.assessment.view
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by pieter on 12/02/18.
+ */
+class AssessmentActivity : AppCompatActivity() {
+}
